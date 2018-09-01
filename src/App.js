5e74e100be77a99beb2b13a434ce38a72e639470
selimssevgi <selimssevgi@gmail.com>
2018-09-01 12:21:37 +0300

    redesign question
    - add static statistics to top
    - change button colors for visual feedback to user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
-import './App.css';
+import { Segment, Container } from 'semantic-ui-react'
 import Question from './page/Question';
+import UserStatistics from './component/UserStatistics';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Question/>
-      </div>
+        <Container>
+          <Segment.Group>
+            <Segment>
+              <UserStatistics/>
+            </Segment>
+            <Segment.Group horizontal>
+              <Segment>
+                <Question/>
+              </Segment>
+            </Segment.Group>
+          </Segment.Group>
+        </Container>
     );
   }
 }
--- src/page/Question.js
+++ src/page/Question.js
@@ -1,53 +1,91 @@
 import React, {Component} from 'react';
-import { Card, Container, Image, Button } from 'semantic-ui-react'
+import {Button, Image, Loader, Segment} from 'semantic-ui-react'
 import * as api from '../util/api';
 
 class Question extends Component {
   constructor(props) {
     super(props);
+
+    this.answerQuestion = this.answerQuestion.bind(this);
+
     this.state = {
       question: {},
-      loaded: false
+      showAnswer: false,
+      givenAnswer: '',
+      loaded: false,
+      answerQuestion: this.answerQuestion
     };
+  }
+
+  answerQuestion(givenAnswer) {
+    console.log("given answer: ", givenAnswer);
+    const correctAnswer = this.state.question.correctAnswer;
+    if (givenAnswer === correctAnswer) {
+      console.log("Act on correct answer");
+    } else {
+      console.log("Act on wrong answer")
+    }
+    this.setState({givenAnswer: givenAnswer, showAnswer: true})
   }
 
   componentDidMount() {
     api.newQuestion().then(response => {
-      console.log("Got question: ", response);
       this.setState({question: response, loaded: true})
     });
   }
 
   render() {
+    const buttonSize = 'large';
+
     return (
-      <Container>
+      <div>
         {this.state.loaded ?
-          (<QuestionForm questionData={this.state.question}/>) :
-          (<p>Loading question...</p>)}
-      </Container>
+          (
+            <div>
+              <Button.Group attached='top' size={buttonSize}>
+                {constructButton(0, this.state)}
+                <Button.Or />
+                {constructButton(1, this.state)}
+              </Button.Group>
+              <Segment attached>
+                <Image src={this.state.question.imgUrl} centered width={800}/>
+              </Segment>
+              <Button.Group attached='bottom' size={buttonSize}>
+                {constructButton(2, this.state)}
+                <Button.Or />
+                {constructButton(3, this.state)}
+              </Button.Group>
+            </div>
+          ) :
+          (<Loader active content='Bringing you a new question...' />)
+        }
+      </div>
     );
   }
 }
 
 export default Question;
 
-function QuestionForm(props) {
+function getButtonColor(buttonText, state) {
+  if (state.showAnswer) {
+    if (state.question.correctAnswer === buttonText) {
+      return 'green';
+    }
+    if (state.givenAnswer === buttonText) {
+      return 'red';
+    }
+  }
+  return 'blue';
+}
+
+function constructButton(number, state) {
+  const option = state.question.options[number];
+
   return (
-    <Card fluid>
-      <Image src={props.questionData.imgUrl} />
-      <Card.Content>
-        <Card.Description>
-          <Button.Group>
-            <Button>{props.questionData.options[0]}</Button>
-            <Button.Or />
-            <Button>{props.questionData.options[1]}</Button>
-            <Button.Or />
-            <Button>{props.questionData.options[2]}</Button>
-            <Button.Or />
-            <Button>{props.questionData.options[3]}</Button>
-          </Button.Group>
-        </Card.Description>
-      </Card.Content>
-    </Card>
+    <Button
+      color={getButtonColor(option, state)}
+      onClick={(e) => state.answerQuestion(option, e)} >
+      {option}
+    </Button>
   );
 }
